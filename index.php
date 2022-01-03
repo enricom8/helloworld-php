5d72350b1b225970a31cf2b72840065840ab8c2f
ENRICO JAMES MONTELEONE <enricom@cn.ibm.com>
2022-01-03 15:10:15 +0800

    Initial commit.

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$html = '';
+$html = $html . '<!DOCTYPE html>';
+$html = $html . '<html>';
+$html = $html . '<head>';
+$html = $html . '<title>Hello World!</title>';
+$html = $html . '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">';
+$html = $html . '</head>';
+$html = $html . '<body>';
+$html = $html . '<h1>Hello World!</h1>';
+$html = $html . '</body>';
+$html = $html . '</html>';
+echo $html;
+?>
